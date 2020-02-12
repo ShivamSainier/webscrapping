8318592d82dd25f4faf3fbc6c0b2f48d20aa8ff0
shivam007-ss <ss1907364@gmail.com>
2020-02-12 21:17:02 +0530

    just intro about what is web scraping actually

---
+++ introduction.py
@@ -0,0 +1,8 @@
+#Web Scrapping is the act of automatically downloading a web page's
+#data, extracting very specific information from it, store in database
+#which can be used for further analysis. Web Scrapping is used to
+#extract large amount of data from web. This is a very useful for Data
+#Scientist or an engineer or anyone who is trying to extract data. So
+#one of the most popular web scrapping tools is Python. With the help of
+#Python we scrap the web page and then extract infomation and find
+#useful insights. Image shown below should make things a bit clear.
